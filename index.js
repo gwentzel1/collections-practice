52b003413560258e38c77970bd3f703492d1ebea
Apple <apple@Apples-MacBook-Pro-10.local>
2019-07-15 16:25:18 -0700

    first

---
+++ index.js
@@ -0,0 +1,20 @@
+// var familyMembers = ["Hugh", "Joy", "Bryan", "Hope"];
+//     familyMembers.push("Zephyr");
+//     console.log(familyMembers);
+//     familyMembers.pop("Zephyr");
+//     console.log(familyMembers);
+// //I added my original list of family members then added and removed Zephyr. :(
+//
+// var randomDecimal = Math.random();
+// var random = randomDecimal * 20;
+// var final = Math.floor(random);
+//
+// console.log(randomDecimal);
+// console.log(random);
+// console.log(final);
+
+var candy = ["M&Ms", "Reese's", "Swedish Fish", "Dots", "Chocolates", "Sour Patch Kids"];
+
+for (var i = 0; i <= 5; i++) {
+  console.log(`${candy[i]} are the best!`);
+}
